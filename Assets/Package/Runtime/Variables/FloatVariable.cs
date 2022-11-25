40bf4cca46fce33ab10bdb14cdd283e59bc492d3
Ítalo <italo.ts4895@gmail.com>
2022-11-24 23:48:06 -0300

    Added new public methods for number variables

--- Assets/Package/Runtime/Variables/FloatVariable.cs
+++ Assets/Package/Runtime/Variables/FloatVariable.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 namespace Toblerone.Toolbox {
     [CreateAssetMenu(menuName = "TobleroneBox/Variable/FloatVariable")]
-    public class FloatVariable : GenericVariable<float> { }
+    public class FloatVariable : GenericVariable<float> {
+        public void IncrementValue(float increment) {
+            Value += increment;
+        }
+
+        public void DecrementValue(float increment) {
+            Value -= increment;
+        }
+
+        public void MultiplyValue(float factor) {
+            Value *= factor;
+        }
+
+        public void DivideValue(float divisor) {
+            Value /= divisor;
+        }
+    }
 }
