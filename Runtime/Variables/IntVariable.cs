88fbe3a76d9b167b2c1cedade3f51760157c099d
Ítalo <italo.ts4895@gmail.com>
2022-11-24 23:48:06 -0300

    Added new public methods for number variables

--- Runtime/Variables/IntVariable.cs
+++ Runtime/Variables/IntVariable.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 namespace Toblerone.Toolbox {
     [CreateAssetMenu(menuName = "TobleroneBox/Variable/IntVariable")]
-    public class IntVariable : GenericVariable<int> { }
+    public class IntVariable : GenericVariable<int> {
+        public void IncrementValue(int increment) {
+            Value += increment;
+        }
+
+        public void DecrementValue(int decrement) {
+            Value -= decrement;
+        }
+
+        public void MultiplyValue(int factor) {
+            Value *= factor;
+        }
+
+        public void DivideValue(int divisor) {
+            Value /= divisor;
+        }
+    }
 }
