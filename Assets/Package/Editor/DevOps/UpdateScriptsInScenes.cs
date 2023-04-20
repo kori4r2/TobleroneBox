d68c4742dc06526ed88905dd6aaca18913b96376
Ítalo <italo.ts4895@gmail.com>
2023-04-20 20:43:12 -0300

    Improved logic for updatable scripts

--- Assets/Package/Editor/DevOps/UpdateScriptsInScenes.cs
+++ Assets/Package/Editor/DevOps/UpdateScriptsInScenes.cs
@@ -3,15 +3,22 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityScene = UnityEngine.SceneManagement.Scene;
 using System;
+using UnityEngine;
 
 namespace Toblerone.Toolbox.EditorScripts {
     public class UpdateScriptsInScenes {
-        private static Type[] typesToCheck = { };
+        private static Type[] typesToCheck = { typeof(MonoBehaviour) };
 
         [MenuItem("Tools/TobleroneBox/Update All Updatable Scripts in All Scenes")]
         public static void UpdateScriptsInAllScenes() {
-            if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            if (!EditorUtility.DisplayDialog("UpdateScriptsInAllScenes", "This will go through all MonoBehaviours in all SceneAssets in the project, and may take a very long time. Continue?", "Yes", "No")) {
+                Debug.Log("UpdateScriptsInAllScenes run cancelled");
                 return;
+            }
+            if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
+                Debug.Log("UpdateScriptsInAllScenes run cancelled");
+                return;
+            }
             string[] guidList = AssetDatabase.FindAssets($"t:SceneAsset");
             List<string> scenesToCheck = new List<string>();
             foreach (string guid in guidList) {
@@ -25,12 +32,13 @@ namespace Toblerone.Toolbox.EditorScripts {
                 EditorSceneManager.SaveScene(updatedScene);
             }
             EditorSceneManager.OpenScene(currentScene, OpenSceneMode.Single);
+            Debug.Log("UpdateScriptsInAllScenes run finished");
         }
 
         private static void UpdateAllUpdatables() {
             List<UnityEngine.Object> objectsToCheck = new List<UnityEngine.Object>();
             foreach (Type type in typesToCheck) {
-                objectsToCheck.AddRange(UnityEngine.Object.FindObjectsOfType(type));
+                objectsToCheck.AddRange(UnityEngine.Object.FindObjectsOfType(type, true));
             }
 
             foreach (UnityEngine.Object obj in objectsToCheck) {
