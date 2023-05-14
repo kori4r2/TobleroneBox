41e17b4fa925cc9312406d3c12127ebf59eb96c8
Ítalo <italo.ts4895@gmail.com>
2023-05-14 11:49:58 -0300

    Trying to make basic functionality work on sample

--- Runtime/SceneChanger/SceneChangeController.cs
+++ Runtime/SceneChanger/SceneChangeController.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
-namespace Toblerone.Toolbox {
+namespace Toblerone.Toolbox.SceneManagement {
     public abstract class SceneChangeController : MonoBehaviour {
+        public abstract void Activate();
+        public abstract void Deactivate();
         public abstract void ManageSceneUnloadOperation(AsyncOperation unloadOperation);
         public abstract void ManageSceneLoadOperation(AsyncOperation loadOperation);
     }
