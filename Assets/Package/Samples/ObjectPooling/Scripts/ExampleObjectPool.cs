f60d33bcc9b5e435e30c6430647498360c2842d5
Ítalo <italo.ts4895@gmail.com>
2025-03-03 16:16:17 -0300

    Added object pooling sample

---
+++ Assets/Package/Samples/ObjectPooling/Scripts/ExampleObjectPool.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Toblerone.Toolbox.ObjectPoolingSample {
+    public class ExampleObjectPool : ObjectPool<ExamplePoolableObject> {
+        [SerializeField] ExamplePoolableObject prefab;
+        protected override ExamplePoolableObject ObjectPrefab => prefab;
+        [SerializeField, Range(1, 500)] private int poolExpansionSize = 1;
+
+        protected override void ExpandPool() {
+            ExpandPoolByFixedNumber(poolExpansionSize);
+        }
+    }
+}
